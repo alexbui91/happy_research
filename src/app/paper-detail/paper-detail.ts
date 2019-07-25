6baaefaf1e0604253a3bc9d9cba24740046d01f4
cuongbt91 <cuongbt91@gmail.com>
2019-07-25 11:17:35 +0900

    add paper detail page

---
+++ src/app/paper-detail/paper-detail.ts
@@ -0,0 +1,38 @@
+import {Component, ViewChild, AfterViewInit} from "@angular/core"
+import { Services } from '../app.services'
+import { Globals } from '../globals'
+import { ActivatedRoute, Params } from '@angular/router'
+import { NewFeeds } from '../newfeeds/index'
+import { Paper } from '../models/paper';
+
+@Component({
+    templateUrl: "paper-detail.html",
+})
+
+export class PaperDetail implements AfterViewInit{
+    
+    paper_id : string = ""
+    paper:  Paper = new Paper()
+    @ViewChild(NewFeeds, {static: false}) newfeeds: NewFeeds
+    constructor(private services: Services, private globals: Globals, private route: ActivatedRoute){
+        this.route.params.subscribe((params: Params) => {
+            this.paper_id = params["id"]
+        })
+    }
+
+    ngAfterViewInit(){
+        this.services.getPaperById(this.paper_id).subscribe(
+            res => {
+                if(res["paper"]){
+                    let obj = res["paper"]
+                    for(let x in obj){
+                        this.paper[x] = obj[x]
+                    }
+                    this.paper["active"] = true
+                    this.paper["showing_abstract"] = this.paper.abstract
+                }
+            }
+        )
+    }
+
+}
